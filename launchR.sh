d49526a38d82846e87c79b2b837a1cb235930306
riguyer <riguyer@gmail.com>
2020-02-18 16:11:45 -0500

    Create launchR.sh

---
+++ launchR.sh
@@ -0,0 +1,52 @@
+launchR() {
+	# launch R from a command line compute node on ERISone cluster
+	# (c) Richard A. Guyer, MD, PhD Feb 11, Jan2020
+	#	riguyer@gmail.com rguyer@partners.org
+	#
+	# this function will launch the default R instance within the specified
+	#		conda environment
+	#
+	# To use, copy this function into ~/.bashrc
+	#
+	# default options:
+	mem_needed=8000
+	cores_needed=4
+	conda_env=CondaR3.6
+
+	# handle parameters
+	while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
+	  -m | --mem )
+		shift; mem_needed=$1
+	    	;;
+	  -c | --cores )
+	   	shift; cores_needed=$1
+	    	;;
+	  -e | --env )
+	   	shift; conda_env=$1
+	    	;;
+		-h | --help )
+	    echo This function launches a command line instance of R from a
+			echo compute node on the Partners RC cluster.
+			echo '  '
+			echo 'Usage: launchR <options>'
+			echo parameters:
+			echo '	-h | --help:			show this message'
+			echo '	-m | --mem:				memory required'
+			echo '	-c | --cores:			cores required'
+			echo '	-e | --env:				conda environment (if other than default)'
+			echo '  '
+			kill -INT $$
+	    	;;
+	esac; shift; done
+	if [[ "$1" == '--' ]]; then shift; fi
+
+	echo launching R from conda environment ${conda_env} by user ${USER}
+	echo cores requested= ${cores_needed}
+	echo memory requested= ${mem_needed}
+	echo '   '
+
+	bsub -Ip \
+			-R "rusage[mem=${mem_needed}]" \
+			-n ${cores_needed} \
+			"module load anaconda; source activate ${conda_env}; R;"
+}
